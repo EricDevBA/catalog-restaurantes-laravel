1223832e66aaac1a03c39598f153bce71e695538
Eric Rosário <ericsantos.r@hotmail.com>
2019-08-27 05:38:17 -0300

    Finalizaçao do CRUD

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,12 @@
 */
 
 Route::prefix('admin')->group(function () {
-    Route::get('restaurant', 'Admin\\RestaurantController@index');
+    Route::get('restaurants', 'Admin\\RestaurantController@index');
     Route::get('restaurants/new', 'Admin\\RestaurantController@new')->name('restaurant.new');
     Route::post('restaurants/new', 'Admin\\RestaurantController@store')->name('restaurant.store');
     Route::get('restaurants/edit/{restaurant}', 'Admin\\RestaurantController@edit')->name('restaurant.edit');
     Route::post('restaurants/update/{id}', 'Admin\\RestaurantController@update')->name('restaurant.update');
+    Route::get('restaurants/remove/{id}', 'Admin\\RestaurantController@delete')->name('restaurant.remove');
     });
 
 
